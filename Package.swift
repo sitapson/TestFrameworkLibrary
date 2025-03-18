59e74b59fab3bca70bc23cd87b7b751110aabfa5
Simon Tapson <sitapson@gmail.com>
2025-03-18 02:51:36 +0000

    initial commit

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TestFrameworkLibrary",
+    platforms: [
+        .iOS(.v16) // iOS version target
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TestFrameworkLibrary",
-            targets: ["TestFrameworkLibrary"]),
+            targets: ["TestFramework"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "TestFrameworkLibrary"),
-
+        // The destination of the framework you will obfuscate the code
+        .binaryTarget(name: "TestFramework", path: "./Sources/TestFramework.xcframework")
     ]
 )
